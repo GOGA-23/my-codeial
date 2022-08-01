259c6c2db2c0ebb68454889143bce8363e59b3e9
GOKUL GANDHI(GTU) <gokulgandhi23@gmail.com>
2022-08-01 19:57:55 +0530

    add a controller and a route for home page

---
+++ controllers/home_controller.js
@@ -0,0 +1,6 @@
+module.exports.home = function(req, res){
+  return res.end('<h1>Express is up for codeial and make using express js</h1>')
+}
+
+// syntax
+// module.exports.actionName = function(req, res) {}
