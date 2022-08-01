73923436b497c8bcb85ca6ccbd5e39cf5d2110f3
GOKUL GANDHI(GTU) <gokulgandhi23@gmail.com>
2022-08-01 20:26:34 +0530

    addded a new route and controller

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,11 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 router.get('/', homeController.home);
+router.use('/user' , require('./user'));
+router.use('/post' , require('./post'));
+
 
 module.exports = router;
+
+// for any further routes, access fro here
+// syntax router.use ('/routerName' , require ("./routerFile"));
